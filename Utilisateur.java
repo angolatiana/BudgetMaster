b908e24f5cad90fc2be61c01b42e34706781effd
angolatiana <hei.angolatiana@gmail.com>
2024-08-21 10:00:55 -1200

    fix:modification of utilisateur

--- Utilisateur.java
+++ Utilisateur.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Utilisateur {
     private String nom;
@@ -33,5 +35,35 @@ public class Utilisateur {
 
     public void setDepenses(List<Depenses> depenses) {
         this.depenses = depenses;
+    }
+
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Utilisateur that = (Utilisateur) o;
+        return budjetMensuel == that.budjetMensuel && Objects.equals(nom, that.nom) && Objects.equals(depenses, that.depenses);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nom, budjetMensuel, depenses);
+    }
+
+    public void nouvelleDepense(Depenses depense){
+        depenses.add(depense);
+    }
+
+    public void depenceParCategorie(Categorie categorie){
+        List<Depenses> depenses1= new ArrayList<>();
+        for (Depenses depense : depenses) {
+            if (depense.equals(categorie)){
+                depenses1.add(depense);
+            }
+        }
+        System.out.println(depenses1);
     }
 }
